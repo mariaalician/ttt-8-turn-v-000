9c6642dbc85cc4c65d7feaa0acad191f6d67c6d8
Maria Nunez <mariaalician@gmail.com>
2018-06-02 21:21:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,7 @@
 def turn(board)
-  
+  puts "Please enter 1-9:"
 end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,16 +9,19 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def input_to_index(input)
-input.to_i - 1
+  input.to_i - 1
 end
-   def valid_move?(board, index)
+
+def valid_move?(board, index)
  if index.between?(0, 8) && !position_taken?(board, index) 
    true
  else 
    false
  end
- end
+end
+ 
 def position_taken?(board, value)
   if board[value] == " " || board[value] == "" || board[value] == nil
     false
@@ -25,6 +29,7 @@ def position_taken?(board, value)
     true
   end
 end
+
 def move(array, index, value = "X") 
   array[index] = value
 end
